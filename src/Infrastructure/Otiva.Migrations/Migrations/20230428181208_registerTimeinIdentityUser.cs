9e5acee91b599982221776238fd7e89c77d436c4
pospyra <pos.pyra@yandex.ru>
2023-04-28 21:17:00 +0300

    обновлены миграции

---
+++ src/Infrastructure/Otiva.Migrations/Migrations/20230428181208_registerTimeinIdentityUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Otiva.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class registerTimeinIdentityUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KodBase64",
+                table: "DomainUser");
+
+            migrationBuilder.RenameColumn(
+                name: "RegistrationDateTime",
+                table: "DomainUser",
+                newName: "DateRegistration");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateRegistration",
+                table: "DomainUser",
+                newName: "RegistrationDateTime");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KodBase64",
+                table: "DomainUser",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
